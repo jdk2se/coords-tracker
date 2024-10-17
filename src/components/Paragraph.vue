270bef53dba9cf4f1a7ee994f7b792738d5ea5e2
Evgenii Dudnik <johnydvgu@gmail.com>
2024-10-17 18:51:04 +0200

    basic markup

---
+++ src/components/Paragraph.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ITaskOption } from "@/types/ITaskOption.ts";
+
+defineProps<{
+  task: ITaskOption,
+}>();
+</script>
+
+<template>
+<article>
+  <h3>{{ task.header }}</h3>
+  <div v-html="task.content"></div>
+</article>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/types/ITaskOption.ts
@@ -0,0 +1,4 @@
+export interface ITaskOption {
+  header: string;
+  content: string;
+}
